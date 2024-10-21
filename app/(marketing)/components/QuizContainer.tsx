54da0cfca8870bef17c9bed0ad9affee64624e09
Sem de Jong <semdejo2003@gmail.com>
2024-10-21 20:57:07 +0200

    fixed eslint issues

--- app/(marketing)/components/QuizContainer.tsx
+++ app/(marketing)/components/QuizContainer.tsx
@@ -1,34 +1,40 @@
-import { Icons } from "@/components/icons";
-import Link from "next/link";
-import { ElementType } from "react";
+import { ElementType } from "react"
+import Link from "next/link"
+
+import { Icons } from "@/components/icons"
 
 interface QuizContainerProps {
-    title: string;
-    description: string;
-    Icon: ElementType;
-    buttonText: string;
-    href: string;
+  title: string
+  description: string
+  Icon: ElementType
+  buttonText: string
+  href: string
 }
 
-export default function QuizContainer({title, description, Icon, buttonText, href}: QuizContainerProps) {
-    return(
-        <div className="bg-background relative overflow-hidden rounded-lg border p-2 duration-200 ease-in-out hover:m-1 hover:scale-110 hover:bg-gray-100 ">
-            <div className="flex h-[250px] flex-col items-center justify-between rounded-md p-4 ">
-              <Icon className="size-[40px] text-red-500" />
-              <div className="flex flex-col items-center space-y-2">
-                <h3 className="font-bold ">{title}</h3>
-                <p className="text-muted-foreground text-center text-sm">
-                    {description}
-                </p>
-              </div>
-              <Link 
-                href={`/quizzes/${href}`}       
-                className="mt-3 rounded-full bg-[#ff4545] px-5 py-1.5 text-sm text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#8d3b3b] "
-            >
-                {buttonText}
-            </Link>
-            </div>
-          
-          </div>
-    )
-}    
+export default function QuizContainer({
+  title,
+  description,
+  Icon,
+  buttonText,
+  href,
+}: QuizContainerProps) {
+  return (
+    <div className="relative overflow-hidden rounded-lg border bg-background p-2 duration-200 ease-in-out hover:m-1 hover:scale-110 hover:bg-gray-100 ">
+      <div className="flex h-[250px] flex-col items-center justify-between rounded-md p-4 ">
+        <Icon className="size-[40px] text-red-500" />
+        <div className="flex flex-col items-center space-y-2">
+          <h3 className="font-bold ">{title}</h3>
+          <p className="text-center text-sm text-muted-foreground">
+            {description}
+          </p>
+        </div>
+        <Link
+          href={`/quizzes/${href}`}
+          className="mt-3 rounded-full bg-[#ff4545] px-5 py-1.5 text-sm text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#8d3b3b] "
+        >
+          {buttonText}
+        </Link>
+      </div>
+    </div>
+  )
+}
--- app/(marketing)/quizzes/quiz/page.tsx
+++ app/(marketing)/quizzes/quiz/page.tsx
@@ -1,12 +1,14 @@
 "use client"
-import { Button } from '@/components/ui/button';
-import React, { useState } from 'react';
+
+import React, { useState } from "react"
+
+import { Button } from "@/components/ui/button"
 
 // Define the types for the quiz question structure
 interface QuizQuestion {
-  question: string;
-  options: string[];
-  answer: string;
+  question: string
+  options: string[]
+  answer: string
 }
 
 const quizQuestions: QuizQuestion[] = [
@@ -16,19 +18,21 @@ const quizQuestions: QuizQuestion[] = [
       "Unusual attachments",
       "Urgent language or threats",
       "A legitimate company name but a suspicious email address",
-      "All of the above"
+      "All of the above",
     ],
     answer: "All of the above",
   },
   {
-    question: "If an email asks you to reset your password through a provided link, what should you do?",
+    question:
+      "If an email asks you to reset your password through a provided link, what should you do?",
     options: [
       "Click the link immediately to reset the password",
       "Ignore the email",
       "Verify the request by contacting the company directly using known contact methods",
-      "Reply to the email for clarification"
+      "Reply to the email for clarification",
     ],
-    answer: "Verify the request by contacting the company directly using known contact methods",
+    answer:
+      "Verify the request by contacting the company directly using known contact methods",
   },
   {
     question: "What is the best way to protect yourself from phishing attacks?",
@@ -36,66 +40,65 @@ const quizQuestions: QuizQuestion[] = [
       "Use two-factor authentication (2FA) wherever possible",
       "Ignore all emails from unknown senders",
       "Only use one strong password for all accounts",
-      "Install antivirus software"
+      "Install antivirus software",
     ],
     answer: "Use two-factor authentication (2FA) wherever possible",
   },
-];
+]
 
 export default function Quiz() {
-  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
-  const [score, setScore] = useState<number>(0);
-  const [showScore, setShowScore] = useState<boolean>(false);
-  const [selectedOption, setSelectedOption] = useState<string>("");
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0)
+  const [score, setScore] = useState<number>(0)
+  const [showScore, setShowScore] = useState<boolean>(false)
+  const [selectedOption, setSelectedOption] = useState<string>("")
 
   const handleAnswerOptionClick = (option: string) => {
     if (option === quizQuestions[currentQuestion].answer) {
-      setScore(score + 1);
+      setScore(score + 1)
     }
-    const nextQuestion = currentQuestion + 1;
+    const nextQuestion = currentQuestion + 1
     if (nextQuestion < quizQuestions.length) {
-      setCurrentQuestion(nextQuestion);
-      setSelectedOption("");
+      setCurrentQuestion(nextQuestion)
+      setSelectedOption("")
     } else {
-      setShowScore(true);
+      setShowScore(true)
     }
-  };
+  }
 
   const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption(event.target.value);
-  };
-
-  
+    setSelectedOption(event.target.value)
+  }
 
   return (
     <>
       <div className="quiz-container flex h-auto flex-col items-center justify-center space-y-8  p-8">
-
         {showScore ? (
           <div className="score-section text-center text-2xl font-semibold">
-            You scored {score} out of {quizQuestions.length}.{' '}
+            You scored {score} out of {quizQuestions.length}.{" "}
             {score === quizQuestions.length
-              ? 'Great job! You are phishing aware.'
-              : 'Keep learning to protect yourself from phishing.'}
+              ? "Great job! You are phishing aware."
+              : "Keep learning to protect yourself from phishing."}
           </div>
         ) : (
-          <span className='rounded-lg bg-gray-100 p-16 my-10'>
+          <span className="my-10 rounded-lg bg-gray-100 p-16">
             {/* Title Section */}
             <div className="w-full text-left">
-                    <h1 className="text-4xl font-bold">Quiz #1</h1> {/* Title added here */}
+              <h1 className="text-4xl font-bold">Quiz #1</h1>{" "}
+              {/* Title added here */}
             </div>
             <div className="question-section space-y-4 text-center">
-              <div className="question-count text-xl font-medium my-8">
-                <span>Question {currentQuestion + 1}</span>/{quizQuestions.length}
+              <div className="question-count my-8 text-xl font-medium">
+                <span>Question {currentQuestion + 1}</span>/
+                {quizQuestions.length}
               </div>
               <div className="question-text text-2xl font-semibold">
                 {quizQuestions[currentQuestion].question}
               </div>
             </div>
-  
+
             <div className="answer-section my-10">
               {quizQuestions[currentQuestion].options.map((option, index) => (
-                <label key={index} className="block text-xl my-4">
+                <label key={index} className="my-4 block text-xl">
                   <input
                     className="mr-2"
                     type="radio"
@@ -108,7 +111,7 @@ export default function Quiz() {
                 </label>
               ))}
             </div>
-            <div className="flex justify-center my-10">
+            <div className="my-10 flex justify-center">
               <button
                 className=" align-items: center rounded-full bg-[#ff4545] px-8 py-5 text-lg font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:bg-[#8d3b3b] disabled:opacity-50"
                 onClick={() => handleAnswerOptionClick(selectedOption)}
@@ -121,6 +124,5 @@ export default function Quiz() {
         )}
       </div>
     </>
-  );
+  )
 }
-
