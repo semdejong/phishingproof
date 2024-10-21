aac8410ec6e20fa158b622efd49110b482015fe1
Sem de Jong <semdejo2003@gmail.com>
2024-10-21 21:03:16 +0200

    fixed issue in typing

--- app/(marketing)/quizzes/[quiz]/page.tsx
+++ app/(marketing)/quizzes/[quiz]/page.tsx
@@ -1,55 +1,71 @@
 "use client"
-import { Quiz } from '@/types/quiz';
-import { useParams } from 'next/navigation';
-import React from 'react'
 
-const quizData: Record<string, Quiz> = {
-    PhishingQuiz1: {
-        title: "Phishing Quiz 1",
-        description: "Test your knowledge of phishing attacks.",
-        questions: [
-            {
-                question: "What is phishing?",
-                options: ["A type of fishing", "A type of scam", "A type of malware", "A type of hacking"],
-                correctAnswer: 1,
-            },
-            {
-                question: "What is the purpose of phishing?",
-                options: ["To steal personal information", "To install malware", "To hack into a system", "To spread fake news"],
-                correctAnswer: 0,
-            },
+import React from "react"
+import { useParams } from "next/navigation"
+
+import { Quiz } from "@/types/quiz"
+
+const quizData: Record<string, any> = {
+  PhishingQuiz1: {
+    title: "Phishing Quiz 1",
+    description: "Test your knowledge of phishing attacks.",
+    questions: [
+      {
+        question: "What is phishing?",
+        options: [
+          "A type of fishing",
+          "A type of scam",
+          "A type of malware",
+          "A type of hacking",
         ],
-    },
-    VishingQuiz2: {
-        title: "Vishing Quiz 2",
-        description: "Test your knowledge of vishing attacks.",
-        questions: [
-            {
-                question: "What is vishing?",
-                options: ["A type of fishing", "A type of scam", "A type of malware", "A type of hacking"],
-                correctAnswer: 1,
-            },
-            {
-                question: "What is the purpose of vishing?",
-                options: ["To steal personal information", "To install malware", "To hack into a system", "To spread fake news"],
-                correctAnswer: 0,
-            },
+        correctAnswer: 1,
+      },
+      {
+        question: "What is the purpose of phishing?",
+        options: [
+          "To steal personal information",
+          "To install malware",
+          "To hack into a system",
+          "To spread fake news",
         ],
-    },
-};
-
-export default function QuizPage() {
-    
-    // const { quiz } = useParams(); // Use useParams to get the dynamic route parameter
-    // const quizDetails = quizData[quiz as string];
-
-    // if (!quizDetails) {
-    //     return <p>Quiz not found.</p>;
-    // }
-  return (
-    <div>
-      
-    </div>
-  )
+        correctAnswer: 0,
+      },
+    ],
+  },
+  VishingQuiz2: {
+    title: "Vishing Quiz 2",
+    description: "Test your knowledge of vishing attacks.",
+    questions: [
+      {
+        question: "What is vishing?",
+        options: [
+          "A type of fishing",
+          "A type of scam",
+          "A type of malware",
+          "A type of hacking",
+        ],
+        correctAnswer: 1,
+      },
+      {
+        question: "What is the purpose of vishing?",
+        options: [
+          "To steal personal information",
+          "To install malware",
+          "To hack into a system",
+          "To spread fake news",
+        ],
+        correctAnswer: 0,
+      },
+    ],
+  },
 }
 
+export default function QuizPage() {
+  // const { quiz } = useParams(); // Use useParams to get the dynamic route parameter
+  // const quizDetails = quizData[quiz as string];
+
+  // if (!quizDetails) {
+  //     return <p>Quiz not found.</p>;
+  // }
+  return <div></div>
+}
--- types/question.ts
+++ types/question.ts
@@ -1,7 +1,7 @@
 export type Question = {
-    question: string;
-    options: string[];
-    answer: string;
-    image?: string;
-    variant: ["regular-mcq", "mcq-with-image", "analyze-email"]; 
-};
+  question: string
+  options: string[]
+  correctAnswer: number
+  image?: string
+  variant: ["regular-mcq", "mcq-with-image", "analyze-email"]
+}
